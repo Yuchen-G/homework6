e91fd3a466b19b2ddb830e40defe5f0fd0014c43
高雨辰 <yc@Yuchens-MacBook-Pro.local>
2020-11-18 14:14:41 +0800

    update

--- js/video.js
+++ js/video.js
@@ -6,9 +6,44 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	console.log("Pause Video");
+	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("slow down to " + video.playbackRate);
+	video.playbackRate *= 0.9;
+});
+
+document.querySelector("#faster").addEventListener("click", function(){
+	console.log("speed up to "+ video.playbackRate);
+	video.playbackRate *= 1.1;
+});
+
+document.querySelector("#skip").addEventListener("click", function(){
+	if (video.duration > video.currentTime + 5) {
+		console.log("skip to " + video.currentTime +" s");
+		video.currentTime += 5;
+	} else {
+		console.log("go back to " + video.currentTime);
+		video.currentTime = 0;
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("muted = " + video.muted);
+	if (video.muted == true) {
+		video.muted = false;
+	} else {
+		video.muted = true;
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("volume changed to " + video.volume);
+	// video.volume = 
+});
